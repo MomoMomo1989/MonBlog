e5449694d2aec0528e0a1e24c880c24fb0a8be2b
Momo <mohandguellal.dev@gmail.com>
2025-02-05 15:26:04 +0100

    khfdsq

---
+++ Unja/UNJA/resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<div class="w-full flex items-center justify-between h-20 navShad px-2 ">
+    <div class="">
+        <img src="{{ asset('images/logo.png') }}" alt="" class="h-[60px] w-[60px]">
+    </div>
+    <button class="sm:hidden block">
+        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+    </button>
+    <div class=" items-center justify-around w-[50%] h-20 sm:flex hidden text-[20px] font-notosans text-gray-600  ">
+        <div class=""> <a href="http://">Accueil</a></div>
+        <div class=""> <a href="http://">A Propos</a></div>
+        <div class=""> <a href="http://">Activitées</a></div>
+        <div class=""> <a href="http://">Projets</a></div>
+        <div class=""> <a href="http://">Contact</a></div>
+    </div>
+</div>
